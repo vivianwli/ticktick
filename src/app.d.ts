3887bcec07f552e5d6a519456c5b2df1d2599462
teacupkittie <vivieli77@gmail.com>
2023-03-09 16:55:51 -0800

    API progress update

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,15 @@
+import type PrismaClient from "@prisma/client";
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
+
+  var prisma: PrismaClient;
 }
 
 export {};
