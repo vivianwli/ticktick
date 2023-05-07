7dc152843c49d32cd51743798bcb2ef20b1b40dc
teacupkittie <vivieli77@gmail.com>
2023-05-07 01:54:06 -0700

    type error hot fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,7 @@
-import type { Task } from "@prisma/client";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ fetch }) => {
   const response = await fetch("/api/tasks");
 
-  return { tasks: (await response.json()) as Task[] };
+  return { tasks: await response.json() };
 };
